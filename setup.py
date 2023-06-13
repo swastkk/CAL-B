ae22cfb9dcef62d7a6787b5d9b866a43737ea20d
Swastik Sharma <81990329+swastkk@users.noreply.github.com>
2023-06-13 16:27:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ setup(
     version= '0.0.1',
     description= 'basiCALC- A basic Calculator python package',
     long_description= open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url='',
+    url='https://github.com/swastkk/CAL-B',
+    project_urls= {
+        Bug Tracker: "https://github.com/swastkk/CAL-B/issues"
+    },
     author= 'Swastik Sharma',
     author_email= 'swastkk@gmail.com',
     license= 'MIT',
